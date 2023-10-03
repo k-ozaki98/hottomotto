15092d729b62c1fe70e4bd8f5e91231695054aa8
k-ozaki98 <112666345+k-ozaki98@users.noreply.github.com>
2023-10-03 12:50:03 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hotto Motto</title>
+  <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <header class="header">
+    <div class="header__logo">
+      <img src="/asset/images/logo_header.png" alt="">
+    </div>
+    <ul class="header__nav">
+      <li>メニュー</li>
+      <li>店舗</li>
+      <li class="active">ネット注文</li>
+      <li>会員サービス</li>
+      <li>その他</li>
+    </ul>
+  </header>
+  <main>
+    <div class="l-inner">
+      <ul class="tab">
+        <li>お弁当</li>
+        <li>おかずのみ</li>
+        <li>サイトメニュー</li>
+      </ul>
+      <section class="content-01 ">
+        <h2 class="sec-ttl">おすすめメニュー（期間限定）</h2>
+        <ul class="menu">
+          <li class="menu__item">
+            <div class="menu__img">
+              <img src="/asset/images/img-01.jpg" alt="">
+            </div>
+            <div class="menu__detail">
+              <p class="menu__name">プラスサラダ 100円サラダ【ドレッシング付き】</p>
+              <p class="menu__price">100円</p>
+
+            </div>
+          </li>
+          <li class="menu__item">
+            <div class="menu__img">
+              <img src="/asset/images/img-01.jpg" alt="">
+            </div>
+            <div class="menu__detail">
+              <p class="menu__name">プラスサラダ 100円サラダ【ドレッシング付き】</p>
+              <p class="menu__price">100円</p>
+
+            </div>
+          </li>
+          <li class="menu__item">
+            <div class="menu__img">
+              <img src="/asset/images/img-01.jpg" alt="">
+            </div>
+            <div class="menu__detail">
+              <p class="menu__name">プラスサラダ 100円サラダ【ドレッシング付き】</p>
+              <p class="menu__price">10０円</p>
+
+            </div>
+          </li>
+          <li class="menu__item">
+            <div class="menu__img">
+              <img src="/asset/images/img-01.jpg" alt="">
+            </div>
+            <div class="menu__detail">
+              <p class="menu__name">プラスサラダ 100円サラダ【ドレッシング付き】</p>
+              <p class="menu__price">100円</p>
+
+            </div>
+          </li>
+        </ul>
+      </section>
+      <section class="content-02">
+
+      </section>
+      <section class="content-03">
+
+      </section>
+    </div>
+  </main>
+</body>
+</html>
